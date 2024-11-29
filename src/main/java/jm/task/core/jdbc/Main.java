e51fc313bb0536fb4ba414d1676367bc8aca4189
LooperDog <[200~nz697448@gmail.com~>
2024-11-29 11:40:56 +0300

    delete version Maven

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,24 +1,25 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-        UserDaoHibernateImpl userDao = new UserDaoHibernateImpl();
+        final UserService userService = new UserServiceImpl();
 
-        userDao.createUsersTable();
+        userService.createUsersTable();
 
-        userDao.saveUser("John", "Doe", (byte) 25);
-        userDao.saveUser("Jane", "Smith", (byte) 30);
-        userDao.saveUser("Michael", "Brown", (byte) 35);
-        userDao.saveUser("Emily", "Davis", (byte) 40);
+        userService.saveUser("John", "Doe", (byte) 25);
+        userService.saveUser("Jane", "Smith", (byte) 30);
+        userService.saveUser("Michael", "Brown", (byte) 35);
+        userService.saveUser("Emily", "Davis", (byte) 40);
 
-        userDao.getAllUsers().forEach(System.out::println);
+        userService.getAllUsers().forEach(System.out::println);
 
-        //userDao.cleanUsersTable();
-        //userDao.dropUsersTable();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
 
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -2,11 +2,7 @@ package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-
-import java.sql.SQLException;
-import java.util.LinkedList;
 import java.util.List;
 
 
@@ -26,7 +22,7 @@ public class UserServiceImpl implements UserService {
         userDao.dropUsersTable();
     }
 
-    public void saveUser(String name, String lastName, byte age) throws SQLException {
+    public void saveUser(String name, String lastName, byte age) {
             userDao.saveUser(name, lastName,age);
     }
 
